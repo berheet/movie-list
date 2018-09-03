ec15bbdb6a4976d69627852b651e1bf2272d5d44
Eyobell <eyobell.berhe@live.longwood.edu>
2018-09-03 00:19:50 -0400

    loading-bar

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Loader = props => (
+    <div> <img src='https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif' alt='loader-icon' style={{width:'75px'}}/></div>
+)
+
+export default Loader;
--- src/components/Series/Series.js
+++ src/components/Series/Series.js
@@ -1,23 +1,35 @@
 import React, {Component} from 'react';
 import SeriesList from '../SeriesList/SeriesList';
+import Loader from '../Loader/Loader';
 
 export default class Series extends Component{
     state = {
-        series: []
+        series: [],
+        seriesName: '',
+        isLoading: false
       }
     
     onSeriesInputChange = e => {
+        this.setState({
+            seriesName: e.target.value, isLoading: true
+        })
         fetch(`http://api.tvmaze.com/search/shows?q=${e.target.value}`)
         .then((response) => response.json())
-        .then(json => this.setState({series:json}))
+        .then(json => this.setState({series:json, isLoading:false}))
           console.log(e)
           console.log(e.target.value)
     }
     render(){
+        const {series, seriesName, isLoading} = this.state;
         return(
             <div>
-                The amount of series': {this.state.series.length}
-                <div> <input type='text' onChange={this.onSeriesInputChange}/></div>
+                <div> <input value = {seriesName} type='text' onChange={this.onSeriesInputChange}/></div>
+                {!isLoading && series.length === 0 && seriesName.trim() === '' &&
+                <p>Please enter a title name </p>
+            
+            }
+            {!isLoading && series.length === 0 && seriesName.trim()  !== '' && <p>No series has been found by this name</p>}
+            {isLoading && <p><Loader/></p>}
                 <SeriesList list={this.state.series}/>
                 </div>
         )
