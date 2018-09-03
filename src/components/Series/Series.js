6a00e63bbf7d9eb53903dfe65ab59a518a603891
Eyobell <eyobell.berhe@live.longwood.edu>
2018-09-02 23:58:49 -0400

    input

--- src/components/Series/Series.js
+++ src/components/Series/Series.js
@@ -6,15 +6,18 @@ export default class Series extends Component{
         series: []
       }
     
-      componentDidMount(){
-        fetch('http://api.tvmaze.com/search/shows?q=Vikings')
+    onSeriesInputChange = e => {
+        fetch(`http://api.tvmaze.com/search/shows?q=${e.target.value}`)
         .then((response) => response.json())
         .then(json => this.setState({series:json}))
-      }
+          console.log(e)
+          console.log(e.target.value)
+    }
     render(){
         return(
             <div>
                 The amount of series': {this.state.series.length}
+                <div> <input type='text' onChange={this.onSeriesInputChange}/></div>
                 <SeriesList list={this.state.series}/>
                 </div>
         )
